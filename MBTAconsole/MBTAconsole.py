2240262b590a6085e00fb4f0e884e502edf9acf9
agover97 <adamgover97@gmail.com>
2023-09-22 15:47:10 -0400

    comment code

--- MBTAconsole/MBTAconsole.py
+++ MBTAconsole/MBTAconsole.py
@@ -6,10 +6,23 @@ from collections import defaultdict, deque
 from itertools import combinations
 
 class Route:
+    """
+    A class used to represent subway route
+    """
     def __init__(self, name, id, stops):
-         self._name = name
-         self._id = id
-         self._stops = stops 
+        """
+        Parameters
+        ----------
+        name : str
+            The long name of the subway route (e.g. "Red Line")
+        id : str
+            The id of the subway route. Used as endpoint for MPTA api. (e.g. "Red")
+        stops : list[Stop]
+            The list of stops on subway line
+        """
+        self._name = name
+        self._id = id
+        self._stops = stops 
       
     def name(self):
         return self._name
@@ -21,7 +34,18 @@ class Route:
         return self._stops
 
 class Stop:
+    """
+    A class used to represent subway stop
+    """
     def __init__(self, name, id):
+        """
+        Parameters
+        ----------
+        name : str
+            The name of the stop (e.g. "Park Street")
+        id : str
+            The id of the stop. Used as endpoint for MPTA api (e.g. "place-pktrm")
+        """
         self._name = name
         self._id = id
 
@@ -33,7 +57,21 @@ class Stop:
 
 __URL__ = "https://api-v3.mbta.com/"
 
+
 def query_MBTA(endpoint):
+    """
+    Submit REST request to MBTA api and handle response
+
+    Parameters
+    ----------
+    endpoint : str
+        endpoint to be appended to base URL
+
+    Returns
+    -------
+    dict
+        json formatted response of request
+    """
     try:
         response = requests.get(__URL__ + endpoint)
         response.raise_for_status()
@@ -44,7 +82,21 @@ def query_MBTA(endpoint):
     else:
         return response.json()
 
+
 def get_routes():
+    """
+    Build list of routes by querying MBTA api
+
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    list[Route]
+        list of route objects representing MBTA subways
+    """
+    #Filter routes on server side so less data is returned
     endpoint = "routes?filter[type]=0,1"
     data = query_MBTA(endpoint)["data"]
     routes = []
@@ -56,6 +108,19 @@ def get_routes():
     return routes
 
 def get_stops(route_id):
+    """
+    Build list of stops
+
+    Parameters
+    ----------
+    route_id : str
+        endpoint of a specific subway line to appended to URL (e.g. "Red")
+
+    Returns
+    -------
+    list[Stops]
+        list of stops on a given subway line
+    """
     endpoint = "stops?filter[route]=" + route_id
     data = query_MBTA(endpoint)["data"]
     stops = []
@@ -66,22 +131,44 @@ def get_stops(route_id):
     return stops
 
 def get_long_names():
+    """
+    Prints the names of all subway line
+    """
     routes = get_routes()
     print("The MBTA has these subway routes:")
     for route in routes:
         print(route.name())
 
 def get_longest_route():
+    """
+    Prints the name and number of stops for the longest subway line
+    """
     routes = get_routes()
     greatest = max(routes, key=lambda route:len(route.stops()))
     print(f"The {greatest.name()} has the greatest number of stops: {len(greatest.stops())} stops")
 
 def get_shortest_route():
+    """
+    Prints the name and number of stops
+    """
     routes = get_routes()
     fewest = min(routes, key=lambda route:len(route.stops()))
     print(f"The {fewest.name()} has the fewest number of stops: {len(fewest.stops())} stops")
 
 def build_stop_dict(routes):
+    """
+    Build a dictionary of all stops on all lines where the key is the stop name and the value is a list of routes that stop is on
+
+    Parameters
+    ----------
+    routes : list[Route]
+        list of Route objects for each subway line
+
+    Returns
+    -------
+    dict{ str : list[str] }
+        dictionary mapping stop name to subway route name
+    """
     stop_dict = defaultdict(list)
     for route in routes:
         for stop in route.stops():
@@ -89,6 +176,18 @@ def build_stop_dict(routes):
     return stop_dict
 
 def get_connecting_stops():
+    """
+    Build a dictionary of all stops that connect two or more subway lines. Calls build_stop_dict and filters results.
+
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    dict{ str : list[str] }
+        dictionary mapping stop name to list of subway route names
+    """
     routes = get_routes()
     stop_dict = build_stop_dict(routes)
     connecting_stops = dict(filter(lambda x:len(x[1])>1, stop_dict.items()))
@@ -97,6 +196,19 @@ def get_connecting_stops():
         print(f"{stop} : {', '.join(connectors)}")
 
 def build_connection_graph(connecting_stops):
+    """
+    Build a dictionary which maps each subway route to all other routes which can be access by sharing a connecting stop
+
+    Parameters
+    ----------
+    connecting_stops : dict{ str : list[str] }
+        dictionary of all stops that connect two or more subway lines
+
+    Returns
+    -------
+    dict{ str : list[str] }
+        dictionary mapping subway route name to list of subway route names
+    """
     connection_graph = defaultdict(set)
     for routes in connecting_stops.values():
         for source, dest in  combinations(routes, 2):
@@ -105,24 +217,59 @@ def build_connection_graph(connecting_stops):
     return connection_graph
 
 def BFS_algo(graph, source, sink):
-	q = deque([[source]])
-	visited = []
-	if source == sink:
-		print("Those are the same stops")
-		return []
-	while q:
-		path = q.popleft()
-		node = path[-1]
-		if node not in visited:
-			for route in graph[node]:
-				new_path = list(path)
-				new_path.append(route)
-				q.append(new_path)
-				if route == sink:
-					return new_path
-		visited.append(node)
+    """
+    Find shortest path from source to sink node along graph
+
+    Parameters
+    ----------
+    graph : dict{ str : list[str] }
+        dictionary representing graph, key is node, value is list of neighbors
+    source : str
+        key of starting node
+    sink : str
+        key of end node
+
+    Returns
+    -------
+    list[str]
+        list of node names comprising shortest path from source to sink
+    """
+    q = deque([[source]])
+    visited = []
+    if source == sink:
+        print("Those are the same stops")
+        return []
+    while q:
+        path = q.popleft()
+        node = path[-1]
+        if node not in visited:
+            for route in graph[node]:
+                new_path = list(path)
+                new_path.append(route)
+                q.append(new_path)
+                if route == sink:
+                    return new_path
+        visited.append(node)
 
 def find_route(start, end):
+    """
+    Find list of subway line changes to get from stop:start to stop:end
+
+    Prints list of changes if valid route is found
+
+    If invalid stop names submitted prints message saying input was invlaid and prints list of all subway stops
+
+    Parameters
+    ----------
+    start : str
+        name of starting stop
+    end : str
+        name of destination stop
+
+    Returns
+    -------
+    none
+    """
     routes = get_routes()
     stop_dict = build_stop_dict(routes)
     connecting_stops = dict(filter(lambda x:len(x[1])>1, stop_dict.items()))
