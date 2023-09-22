dab721c0f18b8f7d056ab9c14730931e421d3320
agover97 <adamgover97@gmail.com>
2023-09-22 16:02:13 -0400

    add unit tests for MBTAconsole functions

--- tests/MBTAconsole_tests.py
+++ tests/MBTAconsole_tests.py
@@ -1,5 +1,5 @@
 import pytest
-from MBTAconsole.MBTAconsole import get_routes
+from MBTAconsole.MBTAconsole import get_routes, build_stop_dict, build_connection_graph
 
 @pytest.fixture()
 def routes():
@@ -10,3 +10,14 @@ def routes():
 class TestResource:
     def test_get_routes(self, routes):
         assert len(routes) == 8
+
+    def test_build_stop_dict(self, routes):
+        stop_dict = build_stop_dict(routes)
+        assert len(stop_dict) > 0
+    
+    def test_build_connection_graph(self, routes):
+        stop_dict = build_stop_dict(routes)
+        connecting_stops = dict(filter(lambda x:len(x[1])>1, stop_dict.items()))
+        connection_graph = build_connection_graph(connecting_stops)
+        assert len(connection_graph) > 0
+        
