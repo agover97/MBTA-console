29d20d42febe206936073725cd58adbfb7abb9c7
agover97 <adamgover97@gmail.com>
2023-09-22 16:38:54 -0400

    docs for cli commands

--- MBTAconsole/cli.py
+++ MBTAconsole/cli.py
@@ -4,26 +4,41 @@ import typer
 
 from MBTAconsole import __app_name__, __version__, MBTAconsole
 
-app = typer.Typer()
+app = typer.Typer(help="Console for MBTA information")
 
 @app.command()
 def show_routes():
+    """
+    Names of all subway stops
+    """
     MBTAconsole.get_long_names()
 
 @app.command()
 def longest_route():
+    """
+    Name and length of longest subway line
+    """
     MBTAconsole.get_longest_route()
 
 @app.command()
 def shortest_route():
+    """
+    Name and length of shortest subway line
+    """
     MBTAconsole.get_shortest_route()
 
 @app.command()
 def connecting_stops():
+    """
+    All subway stops that connect two or more lines
+    """
     MBTAconsole.get_connecting_stops()
 
 @app.command()
 def find_route(start: str, end: str):
+    """
+    Route of subway lines connecting two stops: START, END
+    """
     MBTAconsole.find_route(start, end)
 
 
